66fcca78deb4c47d15fd679bcb0c19454c502a40
Nerdulous5000 <BMWeeks3@gmail.com>
2020-10-20 04:31:31 -0400

    Started implementing items

---
+++ Assets/Scripts/Utils/Item/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Gameplay/Item")]
+public class Item : ScriptableObject
+{
+    public Sprite Sprite;
+    public bool IsEntity = false;
+
+}
