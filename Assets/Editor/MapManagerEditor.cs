01a59b79089fb993782f91a27c1832d4c7bcbe2d
Rane <rane.fields@gmail.com>
2020-10-20 02:45:51 -0700

    Formatting

--- Assets/Editor/MapManagerEditor.cs
+++ Assets/Editor/MapManagerEditor.cs
@@ -4,46 +4,37 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(MapManager))]
-public class MapManagerEditor : Editor
-{
+public class MapManagerEditor : Editor {
     static bool foldout = true;
     static readonly string[] hiddenMapGenerationSettingsProps = new string[] { "m_Script", "BiomeSettings", "ResourceSettings", "HeightmapSettings" };
     static readonly string[] hiddenNoiseSettingsProps = new string[] { "m_Script" };
 
     MapManager manager;
 
-    public override void OnInspectorGUI()
-    {
-        using (var check = new EditorGUI.ChangeCheckScope())
-        {
+    public override void OnInspectorGUI() {
+        using (var check = new EditorGUI.ChangeCheckScope()) {
             base.OnInspectorGUI();
-            if (GUILayout.Button("Regenerate"))
-            {
+            if (GUILayout.Button("Regenerate")) {
                 manager.OnEditorRegeneratePressed();
             }
-            if (manager.GenerationSettings != null)
-            {
+            if (manager.GenerationSettings != null) {
                 DrawSettingsEditor();
             }
-            if (check.changed)
-            {
+            if (check.changed) {
                 manager.OnEditorSettingsUpdate();
             }
         }
     }
 
-    void DrawSettingsEditor()
-    {
+    void DrawSettingsEditor() {
         foldout = EditorGUILayout.InspectorTitlebar(foldout, manager.GenerationSettings);
-        if (foldout)
-        {
+        if (foldout) {
             Editor editor = CreateEditor(manager.GenerationSettings);
             editor.OnInspectorGUI();
         }
     }
 
-    void OnEnable()
-    {
+    void OnEnable() {
         manager = (MapManager)target;
     }
 }
