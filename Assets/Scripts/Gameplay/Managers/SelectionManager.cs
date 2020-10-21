a38c81a3e1e252c3597aa14b56b1d8cb6cf24b9b
Nerdulous5000 <BMWeeks3@gmail.com>
2020-10-21 00:39:24 -0400

    Changed SeleciontManager from a static class to a singleton

--- Assets/Scripts/Gameplay/Managers/SelectionManager.cs
+++ Assets/Scripts/Gameplay/Managers/SelectionManager.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public static class SelectionManager {
-    public static Vector2Int HoveredTile {
+public class SelectionManager : MonoBehaviour{
+    public static SelectionManager Instance;
+    void Awake()
+    {
+        Instance = this;
+    }
+    public Vector2Int HoveredTile {
         get {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             return new Vector2Int(Mathf.FloorToInt(mousePos.x), Mathf.FloorToInt(mousePos.y));
         }
     }
+    public Item ActiveItem;
 
 }
--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Item", menuName = "Gameplay/Item")]
-public class Item : ScriptableObject
-{
+public class Item : ScriptableObject {
     public Sprite Sprite;
+    public string name;
     public bool IsEntity = false;
+    public EntityData EntityData;
+    public int Id;
 
 }
