f76ae7c3b09a1181935fddb0a6c43d1f60a63886
Nerdulous5000 <BMWeeks3@gmail.com>
2020-10-27 21:08:12 -0400

    Started item interfaces for entities

---
+++ Assets/Scripts/Entities/ItemInterfaces/ItemInterface.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Unable to be instantiated
+// Must create derivative with defined size 
+public abstract class ItemInterface : ScriptableObject {
+
+
+    public enum IODirection {
+        Null = 0,
+        Input = 1,
+        Output = 2,
+    };
+    Dictionary<Direction, IODirection[]> IOSlots;
+
+    // Through array index goes from left to right.
+    // All other directions are rotated from top *See below
+
+    // Right : Top    ->  Bottom
+    // Top   : Left   ->  Right
+    // Left  : Bottom ->  Top
+    // Down  : Right  ->  Left
+
+    // TODO: Add intuitive GUI to change io
+
+    protected void Initialize(int width, int height) {
+        for (int n = 0; n < width; n++) {
+            IOSlots[Direction.Up] = new IODirection[width];
+            IOSlots[Direction.Down] = new IODirection[width];
+        }
+        for (int n = 0; n < height; n++) {
+            IOSlots[Direction.Left] = new IODirection[height];
+            IOSlots[Direction.Right] = new IODirection[height];
+        }
+    }
+
+    public bool CanAccept(Direction direction, uint index = 0) {
+        if (index >= IOSlots[direction].Length) {
+            return false;
+        }
+        if (IOSlots[direction][index] == IODirection.Input) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public bool CanGive(Direction direction, uint index = 0) {
+        if (index >= IOSlots[direction].Length) {
+            return false;
+        }
+        if (IOSlots[direction][index] == IODirection.Input) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void SetInput(Direction direction, uint index = 0) {
+        if (index >= IOSlots[direction].Length) {
+            return;
+        }
+        IOSlots[direction][index] = IODirection.Input;
+    }
+    public void SetOutput(Direction direction, uint index = 0) {
+        if (index >= IOSlots[direction].Length) {
+            return;
+        }
+        IOSlots[direction][index] = IODirection.Output;
+    }
+
+    public abstract Vector2Int GetTile(Vector2Int position, Direction direction, uint index);
+
+}
+
+
---
+++ Assets/Scripts/Entities/ItemInterfaces/ItemInterface2x2.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemInterface", menuName = "Gameplay/ItemInterfaces/2x2")] 
+public class ItemInterface2x2 : ItemInterface{
+    // [] []
+    // [] []
+
+    [Header("Right")]
+    public IODirection right0;
+    public IODirection right1;
+    [Header("Up")]
+    public IODirection up0;
+    public IODirection up1;
+    [Header("Left")]
+    public IODirection left0;
+    public IODirection left1;
+    [Header("Down")]
+    public IODirection down0;
+    public IODirection down1;
+
+    void OnEnable()
+    {
+        Initialize(2, 2);
+        // Right
+        if (right0 == IODirection.Input) {
+            SetInput(Direction.Right, 0);
+        } else if (right0 == IODirection.Output) {
+            SetOutput(Direction.Right, 0);
+        }
+        if (right1 == IODirection.Input) {
+            SetInput(Direction.Right, 1);
+        } else if (right1 == IODirection.Output) {
+            SetOutput(Direction.Right, 1);
+        }
+        // Up
+        if (up0 == IODirection.Input) {
+            SetInput(Direction.Up, 0);
+        } else if (up0 == IODirection.Output) {
+            SetOutput(Direction.Up, 0);
+        }
+        if (up1 == IODirection.Input) {
+            SetInput(Direction.Up, 1);
+        } else if (up1 == IODirection.Output) {
+            SetOutput(Direction.Up, 1);
+        }
+        // Left
+        if (left0 == IODirection.Input) {
+            SetInput(Direction.Left, 0);
+        } else if (left0 == IODirection.Output) {
+            SetOutput(Direction.Left, 0);
+        }
+        if (left1 == IODirection.Input) {
+            SetInput(Direction.Left, 1);
+        } else if (left1 == IODirection.Output) {
+            SetOutput(Direction.Left, 1);
+        }
+        // Down
+        if (down0 == IODirection.Input) {
+            SetInput(Direction.Down, 0);
+        } else if (down0 == IODirection.Output) {
+            SetOutput(Direction.Down, 0);
+        }
+        if (down1 == IODirection.Input) {
+            SetInput(Direction.Down, 1);
+        } else if (down1 == IODirection.Output) {
+            SetOutput(Direction.Down, 1);
+        }
+    }
+    public override Vector2Int GetTile(Vector2Int position, Direction direction, uint index) {
+        switch (direction) {
+            case Direction.Right:
+                return position + new Vector2Int(1, 0);
+            case Direction.Up:
+                ;
+                return position + new Vector2Int(0, 1);
+            case Direction.Left:
+                ;
+                return position + new Vector2Int(-1, 0);
+            case Direction.Down:
+                ;
+                return position + new Vector2Int(0, -1);
+            default:
+                return new Vector2Int();
+        }
+    }
+
+}
