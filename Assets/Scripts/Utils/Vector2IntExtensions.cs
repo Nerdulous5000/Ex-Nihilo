47c4685fb829464dd5e93b2bbbeb4333dc3db7dc
Nerdulous5000 <BMWeeks3@gmail.com>
2020-10-31 15:02:52 -0400

    Added exxtension for Vector2Int comparison

---
+++ Assets/Scripts/Utils/Vector2IntExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Vector2IntExtensions
+{
+
+    public static bool IsRightFrom(this Vector2Int self, Vector2Int other) {
+        return self.x > other.x;
+    }
+    public static bool IsUpFrom(this Vector2Int self, Vector2Int other) {
+        return self.y > other.y;
+    }
+    public static bool IsLeftFrom(this Vector2Int self, Vector2Int other) {
+        return self.x < other.x;
+    }
+    public static bool IsDownFrom(this Vector2Int self, Vector2Int other) {
+        return self.y < other.y;
+    }
+}
