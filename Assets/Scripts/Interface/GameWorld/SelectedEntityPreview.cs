e3bf54f0ced61c964dc70f02e46f3a800c9ba9a6
Nerdulous5000 <BMWeeks3@gmail.com>
2020-11-05 17:51:32 -0500

    Selected entity shows up as ghost image on the map

---
+++ Assets/Scripts/Interface/GameWorld/SelectedEntityPreview.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedEntityPreview : MonoBehaviour
+{
+    public bool Enabled = true;
+    SpriteRenderer spriteRenderer;
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Item activeItem = SelectionManager.Instance.ActiveItem;
+        if (Enabled && activeItem != null) {
+            if(activeItem.IsEntity) {
+                spriteRenderer.sprite = activeItem.Sprite;
+                Vector2Int pos = SelectionManager.Instance.HoveredTile;
+                transform.position = new Vector3(pos.x, pos.y, 0) + new Vector3(.5f, .5f, 0);
+            }
+        }
+    }
+    public void Enable() {
+        spriteRenderer.enabled = true;
+        Enabled = true;
+    }
+    public void Disable() {
+        spriteRenderer.enabled = false;
+        Enabled = false;
+    }
+}
