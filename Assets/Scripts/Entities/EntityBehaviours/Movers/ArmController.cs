057c964ab9ebafffcdc741d88e1b6082266a4377
Nerdulous5000 <BMWeeks3@gmail.com>
2020-12-13 16:27:29 -0500

    Basic new model for mover

---
+++ Assets/Scripts/Entities/EntityBehaviours/Movers/ArmController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities.Mover {
+    public class ArmController : MonoBehaviour
+    {
+
+        Mover mover;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mover = transform.parent.gameObject.GetComponent<Mover>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float progress = mover.Progress;
+            transform.rotation = Quaternion.Euler(0, 0, -90.0f * progress);
+        }
+    }
+}
--- Assets/Scripts/Entities/EntityBehaviours/Movers/Mover.cs
+++ Assets/Scripts/Entities/EntityBehaviours/Movers/Mover.cs
@@ -1,60 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Mover : EntityBehaviour {
+namespace Entities.Mover {
+    public class Mover : EntityBehaviour {
 
-    // [SerializeField]
-    // protected float moveDelay;
-    // Timer timer;
+        [SerializeField]
+        [Range(0, 1)]
+        public float Progress = 0;
+        // protected float moveDelay;
+        // Timer timer;
+        protected Timer pullTimer = new Timer();
+        protected Timer pushTimer = new Timer();
 
 
-    // void Start() {
-    //     timer = new Timer();
-    //     timer.Set(moveDelay);
-    //     timer.OnFinish(MoveItem);
-    //     timer.Repeat = true;
-    // }
-    protected int moveRadius = 1;
+        protected enum MoveDirection {
+            CounterClockwise = 0,
+            Clockwise = 1,
+        };
+        protected MoveState currentMoveDirection;
 
-    public override void OnSpawn() {
-        Inventory = new Inventory(1);
-    }
+        protected enum MoveState {
+            Giving = 0,
+            Pulling = 1,
+        };
+        protected MoveState currentMoveState;
 
-    // void FixedUpdate() {
-    //     timer.Tick();
-    // }
-    protected bool PullItem(Direction localDirection) {
-        Vector2Int pullLocation = Position + (DirectionLocalToGlobal(localDirection).ToVector2Int() * moveRadius);
-        if (!EntityManager.Instance.IsNullAt(pullLocation) && Inventory.IsEmpty()) {
-            ItemStack heldStack = EntityManager.Instance.At(pullLocation).Inventory.Pull();
-            if(heldStack == null) {
+        protected int moveRadius = 1;
+
+
+
+        public override void OnSpawn() {
+            Inventory = new Inventory(1);
+            CanGiveItems = false;
+        }
+
+        protected bool PullItem(Direction localDirection) {
+            Vector2Int pullLocation = Position + (DirectionLocalToGlobal(localDirection).ToVector2Int() * moveRadius);
+            if (!EntityManager.Instance.IsNullAt(pullLocation) && Inventory.IsEmpty()) {
+                if(EntityManager.Instance.At(pullLocation).CanAcceptItems) {
+                    ItemStack heldStack = EntityManager.Instance.At(pullLocation).Inventory.Pull();
+                    if (heldStack == null) {
+                        return false;
+                    }
+                    return Inventory.Give(heldStack);
+                }
+                else {
+                    return false;
+                }
+
+            } else {
                 return false;
             }
-            return Inventory.Give(heldStack);
         }
-        else {
-            return false;
+
+        protected bool GiveItem(Direction localDirection) {
+            Vector2Int giveLocation = Position + (DirectionLocalToGlobal(localDirection).ToVector2Int() * moveRadius);
+            if (!EntityManager.Instance.IsNullAt(giveLocation) && !Inventory.IsEmpty()) {
+                if(EntityManager.Instance.At(giveLocation).CanAcceptItems) {
+                    ItemStack heldStack = Inventory.Pull();
+                    return EntityManager.Instance.At(giveLocation).Inventory.Give(heldStack);
+                }
+                else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
+
+        protected void UpdateRotation() {
+
+            Vector3 handPos = Quaternion.Euler(0, 0, -90 * Progress) * Vector2.up * (1 + moveRadius);
+            // GameObject arm = GameObject.Find("Arm");
+            // arm.transform.rotation = Quaternion.Euler(0, 0, -90.0f * Progress);
+            Debug.DrawRay(new Vector3(Position.x, Position.y, 0), handPos, Color.magenta);
+        }
+
+        void Update() {
+            UpdateRotation();
+            Progress = Mathf.Sin(Time.frameCount / 50);
+        }
+        // void MoveItem() {
+
+
+        //     if(PullItem(Direction.Up))
+        //         Debug.Log("Pulled Item.");
+        //     if(GiveItem(Direction.Right))
+        //         Debug.Log("Gave Item.");
+        // }
+
     }
-
-    protected bool GiveItem(Direction localDirection) {
-        Vector2Int giveLocation = Position + (DirectionLocalToGlobal(localDirection).ToVector2Int() * moveRadius);
-        if (!EntityManager.Instance.IsNullAt(giveLocation) && !Inventory.IsEmpty()) {
-            ItemStack heldStack = Inventory.Pull();
-            return EntityManager.Instance.At(giveLocation).Inventory.Give(heldStack);
-        } else {
-            return false;
-        }
-    }
-
-    // void MoveItem() {
-
-
-    //     if(PullItem(Direction.Up))
-    //         Debug.Log("Pulled Item.");
-    //     if(GiveItem(Direction.Right))
-    //         Debug.Log("Gave Item.");
-    // }
-
 }
